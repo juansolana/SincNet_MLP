da5ff4e36e60fcf0c46dd457d98312e7df40dbb0
Juan Solana <jsolanacasas@gmail.com>
2019-03-20 11:39:29 +0000

    Update run.py

--- run.py
+++ run.py
@@ -80,7 +80,8 @@ def load_checkpoint(save_dir, restore_file, model, optimizer):
 				save_checkpoint.best_loss = state_dict['best_loss']
 				save_checkpoint.last_epoch = state_dict['last_epoch']
 				print('Loaded checkpoint {}'.format(checkpoint_path))
-				print('Best MAE Score {}'.format(state_dict['best_mae_loss'])
+				print('Best MAE Loss {}'.format(state_dict['best_mae_loss']))
+				print('Best Loss {}'.format(state_dict['best_loss']))
 				return state_dict
 
 
